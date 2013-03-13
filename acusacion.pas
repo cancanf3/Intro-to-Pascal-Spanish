c682677e06b059519d4e148a392a7283007cfe10
Jouvash <cancanf4@gmail.com>
2013-03-13 11:37:43 -0430

    Trabajando en acusacion

--- acusacion.pas
+++ acusacion.pas
@@ -29,7 +29,59 @@ Begin
 
     (* Acusacion del arma *)
 
-    Writeln('
+    Writeln(' Que arma se uso para asesinarlo? ');
+
+    Writeln(' Armas descartadas ');
+    For i := 0 to ( 5 - jugadorTurno.conta.arma ) Do
+    Begin
+        Writeln(jugadorTurno.lista.arma[i]);
+    End;
+    Writeln(' Armas para acusar');
+    For i := 15 to 20 Do
+    Begin
+        Writeln( i - 14,'.- ',phaInicio[i]);
+    End;
+    Repeat
+    Begin
+        Readln(m);
+    End
+    Until ( n < 7 ) and ( n > 0 ); 
+
+    acus.arma := phaInicio[m+14];
+
+    (* Acusacion de la habitacion *)
+
+    Writeln(' En que lugar crees que ocurrio el asesinato?' );
+
+    Writeln('Habitaciones descartadas ');
+    For i := 0 to 8 Do
+    Begin
+        Writeln(jugadorTurno.lista.habt[i]);
+    End;
+    Writeln(' Habitaciones donde acusar ');
+    For i := 6 to 14 Do
+    Begin
+        Writeln(i - 5,'.- ',phaInicio[i]);
+    End;
+    Repeat
+    Begin
+        Readln(h);
+    End
+    Until ( h > 0 ) and ( h < 10 );
+    
+    acus.habt := phaInicio[h+5];
+
+    (* Verificacion de la acusacion *)
+
+     
+
+
+
+
+
+
+
+
 
 
 End;
